10e2db020864c435bef15694f9c87f4b7183801b
ELDLC18 <josuefabrizioelias@gmail.com>
2025-04-11 16:35:44 -0500

    Comentario de "Grupo3"

--- Ejercicio.py
+++ Ejercicio.py
@@ -28,3 +28,11 @@ print(f"La cantidad de votos del candidato 1 es {candidato1} y el porcentaje que
 print(f"La cantidad de votos del candidato 2 es {candidato2} y el porcentaje que obtuvo es {p2}" ) 
 print(f"La cantidad de votos del candidato 3 es {candidato3} y el porcentaje que obtuvo es {p3}" ) 
 print(f"La cantidad de votos del candidato 4 es {candidato4} y el porcentaje que obtuvo es {p4}" )
+
+"""GRUPO 3:
+-ELIAS DE LA CRUZ, JOSUE FABRIZIO
+-GARCIA TORIBIO, HAMER JOEL
+-MENDOZA HUAMAN, RODRIGO ANDERSON
+-OQUENDO CLEMENTE, WILLIAM DIEGO
+-RIVERA GAMARRA, JHOVANNI DONOVAN
+"""
